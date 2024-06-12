a36de38d3547327ed2cdc016edfa43b2135f8585
dmharrison <dmharrison2014@gmail.com>
2024-06-11 20:24:20 -0400

    created a recipe schema model

---
+++ server/models/Recipe.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const recipeSchema = new Schema({
+    label:{
+        type: String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    source:{
+        type:String,        
+    },
+    url:{
+        type:String
+    },
+    ingredients:[{
+        type:Schema.Types.ObjectId,
+        ref:'Ingredient'
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+        get: (timestamp) => dateFormat(timestamp)
+    }
+});
+
+const Recipe = model("Recipe", recipeSchema)
+
+module.exports = Recipe;
