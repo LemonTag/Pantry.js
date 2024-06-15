b497ee83840da65ab280e01eca622d86f3eec3a2
dmharrison <dmharrison2014@gmail.com>
2024-06-14 21:29:14 -0400

    please

--- client/src/components/Pantry/PantryList.jsx
+++ client/src/components/Pantry/PantryList.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
-import { Checkbox, Button, Container, Typography, List, ListItem, ListItemText, ListItemSecondaryAction, FormControlLabel, IconButton, Menu, MenuItem } from '@mui/material';
+import React, { useState, } from 'react';
+import { Checkbox, Button, Container, Typography, List, ListItem, ListItemText, ListItemSecondaryAction, FormControlLabel, IconButton, Menu, MenuItem, Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ALL_INGREDIENTS } from '../../utils/queries';
-import { DELETE_INGREDIENT } from '../../utils/mutations';
+import { DELETE_INGREDIENT, UPDATE_INGREDIENT } from '../../utils/mutations';
 import { useNavigate } from 'react-router-dom';
-import AuthService from '../../utils/auth';
-import SearchModal from './SearchModal';
 
 const PantryList = () => {
   const { loading, error, data } = useQuery(GET_ALL_INGREDIENTS);
+  const [updateIngredient] = useMutation(UPDATE_INGREDIENT, {
+    refetchQueries: [{ query: GET_ALL_INGREDIENTS }],
+  });
+
+
   const [deleteIngredient] = useMutation(DELETE_INGREDIENT,{
     update(cache, { data: { deleteIngredient } }) {
       const { getAllIngredients } = cache.readQuery({ query: GET_ALL_INGREDIENTS});
@@ -24,13 +31,12 @@ const PantryList = () => {
       });
     },
   });
-
+  
+  const [openDialog, setOpenDialog] = useState(false);
   const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [editedIngredient, setEditedIngredient] = useState(null);
   const [anchorEl, setAnchorEl] = useState(null);
   const [currentIngredientId, setCurrentIngredientId] = useState(null);
-  const [modalOpen, setModalOpen] = useState(false); // State to control modal open/close
-  const [searchResults, setSearchResults] = useState([]); // State to hold search results
-  const [errorMessage, setErrorMessage] = useState(''); // State for error message
   const navigate = useNavigate();
 
   if (loading) return <p>Loading...</p>;
@@ -49,53 +55,8 @@ const PantryList = () => {
     setSelectedIngredients(newSelected);
   };
 
-  const handleSearch = async () => {
-    try {
-      // Ensure AuthService is defined before using it
-      if (AuthService) {
-        const isLoggedIn = AuthService.loggedIn();
-        if (!isLoggedIn) {
-          setErrorMessage('Must be logged in');
-          return;
-        }
-      } else {
-        console.error('AuthService is not defined.');
-        setErrorMessage('Authentication service is not available.');
-        return;
-      }
-
-      // Get selected ingredient names to use as keywords
-      const selectedIngredientNames = data.getAllIngredients
-        .filter(ingredient => selectedIngredients.includes(ingredient._id))
-        .map(ingredient => ingredient.food);
-
-      if (selectedIngredientNames.length === 0) {
-        setErrorMessage('Select at least one ingredient to search.');
-        return;
-      }
-
-      // Construct query string with selected ingredient names
-      const queryString = selectedIngredientNames.join('+');
-
-      // Perform API request to Edamam with the constructed query string
-      const response = await fetch(`https://api.edamam.com/search?q=${queryString}&app_id=e60d45ac&app_key=fcb5780894c4282cc330af20f9a037df`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch recipes from Edamam');
-      }
-
-      const responseData = await response.json();
-      const recipes = responseData.hits.map(hit => hit.recipe);
-      setSearchResults(recipes);
-      setModalOpen(true); // Open the modal with search results
-    } catch (error) {
-      console.error('Error fetching recipes:', error);
-      setErrorMessage('Failed to fetch recipes');
-    }
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false); // Close the modal
-    setSearchResults([]); // Clear search results
+  const handleSearch = () => {
+    navigate('/cookbook', { state: { ingredientIds: selectedIngredients } });
   };
 
   const handleMenuClick = (event, id) => {
@@ -108,13 +69,52 @@ const PantryList = () => {
     setCurrentIngredientId(null);
   };
 
+ 
   const handleDelete = async () => {
-    try {
-      await deleteIngredient({ variables: { _id: currentIngredientId } });
+      try {
+      await deleteIngredient({ variables: { _id: currentIngredientId} });
+      
       handleMenuClose();
     } catch (error) {
       console.error('Error deleting ingredient:', error);
     }
+    
+  };
+
+  const handleOpenDialog = (ingredient) => {
+    setEditedIngredient(ingredient);
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setEditedIngredient(null);
+  };
+
+  const handleUpdateIngredient = async () => {
+    try {
+      
+      await updateIngredient({
+        variables: {
+          _id: editedIngredient._id,
+          food: editedIngredient.food,
+          quantity: parseInt(editedIngredient.quantity),
+          measure: editedIngredient.measure,
+          
+        },
+      });
+      handleCloseDialog();
+    } catch (error) {
+      console.error('Error updating ingredient:', error);
+    }
+  };
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedIngredient({
+      ...editedIngredient,
+      [name]: value,
+    });
   };
 
   return (
@@ -125,7 +125,7 @@ const PantryList = () => {
       <div style={{ maxHeight: '800px', overflowY: 'auto' }}>
         <List>
           {data.getAllIngredients.map((ingredient) => (
-            <ListItem key={ingredient._id} button>
+            <ListItem key={ingredient._id} button onClick={() => handleOpenDialog(ingredient)}>
               <ListItemText
                 primary={ingredient.food}
                 secondary={`Quantity: ${ingredient.quantity || ''} ${ingredient.measure || ''}`}
@@ -147,8 +147,8 @@ const PantryList = () => {
                   open={Boolean(anchorEl)}
                   onClose={handleMenuClose}
                 >
-                  <MenuItem onClick={handleMenuClose}>Update Quantity</MenuItem>
-                  <MenuItem onClick={handleDelete}>Delete</MenuItem>
+                  <MenuItem onClick={() => handleOpenDialog(ingredient)}>Update</MenuItem>
+                  <MenuItem onClick={() => handleDelete(ingredient._id)}>Delete</MenuItem>
                 </Menu>
               </ListItemSecondaryAction>
             </ListItem>
@@ -159,11 +159,49 @@ const PantryList = () => {
         Search
       </Button>
 
-      {/* Render the SearchModal component */}
-      <SearchModal isOpen={modalOpen} onClose={handleCloseModal} searchResults={searchResults} />
-
-      {/* Display error message */}
-      {errorMessage && <p>{errorMessage}</p>}
+      {editedIngredient && (
+        <Dialog open={openDialog} onClose={handleCloseDialog}>
+          <DialogTitle>Update Ingredient</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="food"
+              label="Food"
+              type="text"
+              fullWidth
+              value={editedIngredient.food || ""}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="quantity"
+              label="Quantity"
+              type="number"
+              fullWidth
+              value={editedIngredient.quantity || ""}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="measure"
+              label="Measure"
+              type="text"
+              fullWidth
+              value={editedIngredient.measure || ""}
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleUpdateIngredient} color="primary">
+              Save Update
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </Container>
   );
 };
